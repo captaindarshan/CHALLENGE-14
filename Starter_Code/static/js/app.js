7ad0d9ac5e87c0fd226a1cc9d4d3fd551d4afeb2
captaindarshan <39537319+captaindarshan@users.noreply.github.com>
2024-06-22 22:43:37 -0400

    edits

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,9 +1,11 @@
 // Build the metadata panel
+
+
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    
 
     // Filter the metadata for the object with the desired sample number
 
